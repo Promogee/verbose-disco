cf9a70a0775091ac2337dffeda4da9e5a3d432d7
promise <icesoluwalademi@gmail.com>
2025-03-25 06:57:12 +0000

    implement complete and error handling

--- new.py
+++ new.py
@@ -7,7 +7,8 @@ input_box=sc.InputText(tooltip='enter a todo',key='todo')
 list_box=sc.Listbox(values=get_todo(),key='todos',enable_events=True,size=[45,15])
 button=sc.Button('add')
 edit_button=sc.Button('edit')
-window=sc.Window('My todo App',layout=[[label],[input_box,button],[list_box,edit_button]],font=('sans-serif',15))
+complete_button=sc.Button('complete')
+window=sc.Window('My todo App',layout=[[label],[input_box,button],[list_box,edit_button],[complete_button]],font=('sans-serif',15))
 
 while True:
     event,values=window.read()
@@ -15,23 +16,39 @@ while True:
     print(values)
     match event:
         case 'add':
-            todos=get_todo()
             new_todo=values['todo']+'\n'
             todos.append(new_todo)
             set_todo(todos)
             window['todos'].update(values=todos)
         case 'edit':
-            new_todo=values['todo']
-            print(new_todo)
-            todo_to_edit=values['todos'][0]
-            print(todo_to_edit)
-            index_of_todo=todos.index(todo_to_edit)
-            todos[index_of_todo]=new_todo+'\n'
-            set_todo(todos)
-            window['todos'].update(values=todos)
+            try:
+                new_todo=values['todo']
+                print(new_todo)
+                todo_to_edit=values['todos'][0]
+                print(todo_to_edit)
+                index_of_todo=todos.index(todo_to_edit)
+                todos[index_of_todo]=new_todo+'\n'
+                set_todo(todos)
+                window['todos'].update(values=todos)
+                # to display the selected todo in the input-box
+            except IndexError:
+                sc.popup('please select an item',font=('sans-serif',20))
+
+
+
+
         case 'todos':
             window['todo'].update(value=values['todos'][0])
-
+        case 'complete':
+            try:
+                completed_todo=values['todos'][0]
+                todos.remove(completed_todo)
+                set_todo(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sc.popup('please select an item',font=('sans-serif',20))
+        case sc.WIN_CLOSED:
+            break
 window.close()
 # label=sc.Text('enter feet')
 # label2=sc.Text('enter inches')
