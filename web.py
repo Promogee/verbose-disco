5549ecae72e720224b3f396fd5ea9891103a0d13
promise <icesoluwalademi@gmail.com>
2025-03-27 08:36:36 +0000

    before fix dupliocate error

--- web.py
+++ web.py
@@ -1,20 +1,17 @@
+import sqlite3
+
 import streamlit as st
 import function
-
 todos=function.get_todo()
 st.title('My Todo App')
 def add_todo():
     todo=st.session_state['new_todo']+'\n'
-    for item in todos:
-        if item ==todo:
-            st.toast('you cannot put the same todo twice')
-
-        else:
-            todos.append(todo)
-            function.set_todo(todos)
+    todos.append(todo)
+    function.set_todo(todos)
 
 for index, todo in enumerate(todos):
     checkbox=st.checkbox(todo,key=todo)
+    print(checkbox)
     if checkbox:
         todos.pop(index)
         function.set_todo(todos)
